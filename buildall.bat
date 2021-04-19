197d02e9bda7b7a935250e2346f1e42aadbfdf95
Eric Chanudet <chanudete@ainfosec.com>
2021-04-19 12:29:58 -0700

    buildall: stop building if one module failed to
    Do not try to build remaining drivers and installer if one failed along
    the way.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- buildall.bat
+++ buildall.bat
@@ -1,11 +1,14 @@
 @echo off
 
+setlocal enabledelayedexpansion
 set type=%1
 
-for %%x in (xenbus, xenvusb, xenusbdevice, ivc, pv-display-helper, glasswddm) do (
+for %%x in (xenbus, xencons, xenhid, xeniface, xennet, xenvbd, xenvif, xenvkbd, xenvusb, xenusbdevice, ivc, pv-display-helper, glasswddm) do (
  pushd %%x
  powershell ./build.ps1 %type% x64
+ set err=!errorlevel!
  popd
+ if !err! neq 0 goto :eof
 )
 
 set type=%type:checked=Debug%
